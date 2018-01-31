bd35c567f35cd9ebef215feb364846cb0e870b56
denise mendez <d.mnedez@accenture.com>
2018-01-31 13:33:09 -0800

    arreglar posicionamiento del div de login en movil

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { LoginService } from '../../services/login/login.service';
 
 @Component({
   selector: 'app-navbar',
@@ -7,7 +8,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+
+  login: boolean = false;
+  constructor(private loginServ: LoginService) {
+   }
+
+   public showLogin(){
+     this.login = !this.login;
+   }
+
+   loginTo(){
+     console.log("logueando");
+     this.loginServ.updateUserStatus({
+      loggedIn: true
+      //username: this.loginForm.get('email').value
+    });
+    this.loginServ.status.subscribe( () => {
+      console.log("redireccionando");
+      //if (data.isLoggedIn === true) this.router.navigateByUrl('/admin/dashboard'):
+    });
+   }
+
+    logout() {
+      console.log('logout...');
+      this.loginServ.updateUserStatus({loggedIn: false});
+    }
 
   ngOnInit() {
   }
