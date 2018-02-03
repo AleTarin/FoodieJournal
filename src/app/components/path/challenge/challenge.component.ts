2ffc96a7425a8f504f34cccc8dcea4c9badb1ea4
Alejandro De la Cruz Tarin <ale.tarin10@gmail.com>
2018-02-02 22:13:49 -0600

    fix acordeon on challenge

--- src/app/components/path/challenge/challenge.component.ts
+++ src/app/components/path/challenge/challenge.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, OnChanges, Input, SimpleChanges } from '@angular/core';
 import { YelpService } from '../../../services/yelp.service';
 import { Track } from '../../../interfaces/track';
 import { Business } from '../../../interfaces/business';
@@ -8,7 +8,8 @@ import { Business } from '../../../interfaces/business';
   templateUrl: './challenge.component.html',
   styleUrls: ['./challenge.component.scss']
 })
-export class ChallengeComponent implements OnInit {
+export class ChallengeComponent implements OnInit, OnChanges {
+
   latitude: number;
   longitude: number;
   ArrayBs: Business[];
@@ -16,22 +17,27 @@ export class ChallengeComponent implements OnInit {
 
   constructor(private yelpService: YelpService) { }
 
-  ngOnInit() {
-    if (window.navigator.geolocation) {
-      window.navigator.geolocation.getCurrentPosition(position => {
-        // Save the latitude to use
-        this.latitude = position.coords.latitude;
-        this.longitude = position.coords.longitude;
-        console.log(this.path);
-
-        // Search by location and categorie, the radius can change
-        this.yelpService.YelpSearch(this.latitude, this.longitude, this.path.categories, 10000)
-        .subscribe(res => {
-          // Save the bussinesses to use in the template
-          this.ArrayBs = res;
-          console.log(this.ArrayBs);
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes.path.currentValue !== undefined) {
+      if (window.navigator.geolocation) {
+        window.navigator.geolocation.getCurrentPosition(position => {
+          // Save the latitude to use
+          this.latitude = position.coords.latitude;
+          this.longitude = position.coords.longitude;
+          console.log(this.path);
+          // Search by location and categorie, the radius can change
+          this.yelpService.YelpSearch(this.latitude, this.longitude, this.path.categories, 10000)
+          .subscribe(res => {
+            // Save the bussinesses to use in the template
+            this.ArrayBs = res;
+            console.log(this.ArrayBs);
+          });
         });
-      });
+      }
     }
+  }
+
+  ngOnInit() {
+
   }
 }
