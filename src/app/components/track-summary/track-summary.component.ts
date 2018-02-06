27aafbf95bdbba542da45ad9c5bcf7a66a73f591
Jonathan Garza <jonathan.garza@accenture.com>
2018-02-06 09:11:09 -0600

    hi

--- src/app/components/track-summary/track-summary.component.ts
+++ src/app/components/track-summary/track-summary.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Track } from '../../interfaces/track';
 import { YelpService } from '../../services/yelp.service';
 import { PathsService } from '../../services/paths.service';
+import { forEach } from '@angular/router/src/utils/collection';
 
 @Component({
   selector: 'app-track-summary',
@@ -12,9 +13,24 @@ export class TrackSummaryComponent implements OnInit {
   longitude: number;
   latitude: number;
 
+
+ 
+
   trackArray: Track[];
 
   constructor(private yelpService: YelpService, private pathService: PathsService ) { }
+
+getProgress(){
+
+
+
+}
+styleBar(){
+
+
+  
+}
+
 
   ngOnInit() {
     this.pathService.getPathsInfo().subscribe(res => this.trackArray = <Track[]>res);
