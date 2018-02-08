973dd6c7cfe935119e691461f619c30b47d191df
denise mendez <d.mnedez@accenture.com>
2018-02-07 22:48:12 -0600

    creating the object first try

--- src/app/components/shared/status-button/status-button.component.ts
+++ src/app/components/shared/status-button/status-button.component.ts
@@ -1,16 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnChanges, Input, SimpleChanges } from '@angular/core';
+import { User } from '../../../user';
+import { AuthService } from '../../../services/auth.service';
 
 @Component({
   selector: 'app-status-button',
   templateUrl: './status-button.component.html',
   styleUrls: ['./status-button.component.scss']
 })
-export class StatusButtonComponent implements OnInit {
-  hasJourney: boolean = true;
-  isCompleted: boolean = true;
-  buttonLabel: string = "";
+export class StatusButtonComponent implements OnInit, OnChanges {
 
-  constructor() { 
+  hasJourney: boolean;
+  isCompleted: boolean;
+  buttonLabel: string = "";
+  @Input() progress : number;
+
+  ngOnChanges(changes: SimpleChanges): void {
+      if(changes.progress.currentValue !==undefined){
+        console.log(changes.progress.currentValue);
+      }
+    }
+
+  constructor(private auth: AuthService) { 
+    this.journeyAssgned();
+    this.hasCompleted();
     this.checkCurrentStatus();
   }
 
@@ -28,7 +40,59 @@ export class StatusButtonComponent implements OnInit {
     }
   }
 
-  ngOnInit() {
+  public journeyAssgned(){
+    this.auth.userProfile$.subscribe(profile => {
+      const myProfile = <User>profile;
+      if(!myProfile.last_challenge && !myProfile.journey){
+        this.hasJourney = false; 
+      }else{
+        this.hasJourney = true;
+      }
+    });
   }
 
+  public hasCompleted(){
+    if(this.progress !== 100){
+      this.isCompleted = false;
+    }else{
+      this.isCompleted = true;
+    }
+  }
+
+  public chooseJourney() {
+    console.log("fdsfsds");
+    //if(this.buttonLabel === "Follow"){
+      this.auth.userProfile$.subscribe(res => {
+        console.log("dentro");
+        let myProfile: any = <User>res;
+        console.log(<User>res);
+        let userChoose = new User();
+        userChoose.name = myProfile.name;
+        userChoose.nickname = myProfile.nickname;
+        userChoose.picture = userChoose.picture;
+        userChoose.journey = true;
+        console.log(userChoose);
+        localStorage.setItem('profile2', JSON.stringify(userChoose));
+      //}
+    });
+
+    let userChoose = new User();
+        userChoose.name = "bla"
+        userChoose.nickname = "bla"
+        userChoose.picture = "pla"
+        userChoose.journey = true;
+        console.log(userChoose);
+        localStorage.setItem('profile', JSON.stringify(userChoose));
+  }
+
+
+
+
+
+  
+
+  ngOnInit() {
+    
+
+}
 }
--- src/app/user.ts
+++ src/app/user.ts
@@ -2,4 +2,7 @@ export class User {
     name: string;
     nickname: string;
     picture: string;
+    journey?: boolean;
+    last_challenge?: string;
+    time_started?: string;
 }
