fc746db192d3e638f36c183e45981072bd4fa8bf
denise mendez <d.mnedez@accenture.com>
2018-02-12 14:21:00 -0600

    continuando con las validadciones del login, una parte

--- src/app/services/auth.guard.spec.integrated.ts
+++ src/app/services/auth.guard.spec.integrated.ts
@@ -1,19 +1,5 @@
-
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
-
-import {Component} from "@angular/core";
-// import { BrowserDynamicTestingModule, platformBrowserDynamicTesting 
-// } from '@angular/platform-browser-dynamic/testing';
-
-
-// import {RouterTestingModule} from "@angular/router/testing";
-// import {Router, RouterModule, Routes} from "@angular/router";
-
-import { AuthService } from './auth.service';
-import { AuthGuard } from './auth.guard';
-import { HomeComponent } from '../components/home/home.component';
-import { AppComponent } from '../../app/app.component'
+import {Component} from '@angular/core';
 
 @Component({ template: '<h1>H</h1>' })
 export class TestComponent {}
@@ -22,23 +8,12 @@ class MockRouter {
     navigate(url: string) { return url; }
 }
 
-// export const routes: Routes = [
-//     {path: '', redirectTo: 'home', pathMatch: 'full'},
-//     {path: 'home', component: HomeComponent}
-//     ];
-
     describe('AppComponent', () => {
         let component: TestComponent;
         let fixture: ComponentFixture<TestComponent>;
-    
         beforeEach(() => {
             TestBed.configureTestingModule({
-                // imports: [],
                 declarations: [TestComponent]
-                // providers: [
-                //     {provide: Router, useClass: MockRouter}
-                // ], 
-                // schemas: []
             }).compileComponents();
             fixture = TestBed.createComponent(TestComponent);
 
@@ -47,6 +22,5 @@ class MockRouter {
         });
 
             it('should ...', () => {
-               
             });
     });
